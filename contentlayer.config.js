c9197430e9dfd0daaeaf5e13893f3b1ff2f29313
yaerhee <yaerhee@naver.com>
2022-12-07 22:34:30 +0900

    contentlayer config - Post 필드는 간결하게, remark & rehype

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,21 +1,19 @@
 // contentlayer.config.js
-
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import {defineDocumentType, makeSource} from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+import rehypeSlug from 'rehype-slug'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrism from 'rehype-prism-plus'
+import {rehypeAccessibleEmojis} from 'rehype-accessible-emojis'
 
 export const Post = defineDocumentType(() => ({
     name: 'Post',
     filePathPattern: `**/*.mdx`,
+    contentType: 'mdx',
     fields: {
-        title: {
-            type: 'string',
-            description: 'The title of the post',
-            required: true,
-        },
-        date: {
-            type: 'date',
-            description: 'The date of the post',
-            required: true,
-        },
+        title: { type: 'string', required: true, },
+        date: { type: 'date', required: true, },
     },
     computedFields: {
         url: {
@@ -28,4 +26,14 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
     contentDirPath: 'posts',
     documentTypes: [Post],
+    mdx: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+            rehypeSlug,
+            rehypeCodeTitles,
+            rehypePrism,
+            rehypeAccessibleEmojis,
+            rehypeAutolinkHeadings,
+        ]
+    }
 })
