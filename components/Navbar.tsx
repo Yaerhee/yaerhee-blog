5a769177642319e0a97117d2ca715ef22ef646db
yaerhee <yaerhee@naver.com>
2022-12-20 23:39:53 +0900

    style: Navbar text style -> gradient

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -54,14 +54,15 @@ export default function Navbar() {
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="hidden sm:ml-6 sm:block">
-                                <div className="flex space-x-4">
+                                <div className="flex space-x-8">
                                     {siteMap.map((item, index) => {
                                         return (
                                             <Link
                                                 key={index}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.href === router.pathname ? 'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white',
+                                                    item.href === router.pathname ? 'text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600'
+                                                        : 'hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600',
                                                     'px-3 py-2 rounded-md text-sm font-medium transition ease-in-out delay-80'
                                                 )}
                                             target={item.name === GITHUB ? '_blank' : ''}>
