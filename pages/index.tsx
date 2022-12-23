b5b267a39c42779d12d0f76e24aa7aedd5c66c0e
yaerhee <yaerhee@naver.com>
2022-12-23 23:55:12 +0900

    style: import Posts on index.tsx
    TODO: 포스트 갯수 slice ?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Seo from "../components/Seo";
+import {allPosts, Post} from "contentlayer/generated";
+import Posts from "./posts";
 
 export default function Home() {
     return (
         <>
             <Seo title="Blog"/>
             <div className="main-container">
+                <Posts posts={allPosts} />
             </div>
         </>
     )
