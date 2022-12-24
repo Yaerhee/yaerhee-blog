bcfb6478e49679847f446dc4abfd66495bc2361a
yaerhee <yaerhee@naver.com>
2022-12-24 17:38:33 +0900

    chore: temp - delete PostLists from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import Seo from "../components/Seo";
-import {allPosts, Post} from "contentlayer/generated";
-import Posts from "./posts";
 
 export default function Home() {
     return (
         <>
             <Seo title="Blog"/>
             <div className="main-container">
-                <Posts posts={allPosts} />
             </div>
         </>
     )
