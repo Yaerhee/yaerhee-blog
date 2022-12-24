e151aa6ca4b0fc4cf48277e3d0ec556f48836709
yaerhee <yaerhee@naver.com>
2022-12-24 17:13:25 +0900

    style: add winter bg image & hide theme toggle
    because it's christmas : D

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -75,7 +75,8 @@ export default function Navbar() {
                         </div>
                         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                             <div className="relative ml-3">
-                                <div className="cursor-pointer icon-effect" onClick={e => HandleTheme(e)}>
+                                {/*<div className="cursor-pointer icon-effect" onClick={e => HandleTheme(e)}>*/}
+                                <div>
                                     {setIconPerEachTheme()}
                                 </div>
                             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import Layout from "../components/Layout";
 export default function App({ Component, pageProps }: AppProps) {
   return (
       // Layout - Navbar with children
-    <Layout>
-        {/* container layout */}
-        <div className="w-2/5 mt-8 mx-auto min-h-[80vh]">
-            <Component {...pageProps} />
-        </div>
-    </Layout>
+      <div id="main_background">
+          <Layout>
+              {/* container layout */}
+              <div className="w-2/5 mt-8 mx-auto min-h-[100vh]">
+                  <Component {...pageProps} />
+              </div>
+          </Layout>
+      </div>
   )
 }
